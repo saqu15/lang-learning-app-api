2ea8e1f788ae81579957169ad7d4176f68183f84
Szymon <szymon071200@gmail.com>
2023-12-15 17:39:50 +0100

    fetching data from database

--- api/routes/wordsets.ts
+++ api/routes/wordsets.ts
@@ -1,35 +1,132 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { Wordset } from '../models/wordset.js';
+import { Word } from '../models/word.js';
 
 export const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'Wordsets were fetched',
-	});
+	Wordset.find()
+		.select('-__v')
+		.populate('word', '-__v')
+		.exec()
+		.then(wordsets => {
+			res.status(200).json({
+				count: wordsets.length,
+				wordsets: wordsets.map(wordset => {
+					return {
+						_id: wordset._id,
+						word: wordset.word,
+						elements: wordset.elements,
+						request: {
+							type: 'GET',
+							url:
+								process.env.APP_URL +
+								'/wordsets/' +
+								wordset._id,
+						},
+					};
+				}),
+			});
+		})
+		.catch(err => {
+			res.status(500).json({ error: err });
+		});
 });
 
 router.post('/', (req, res, next) => {
-	const wordset = {
-		wordId: req.body.wordId,
-		langFrom: req.body.langFrom,
-		langTo: req.body.langTo,
-	};
-	res.status(201).json({
-		message: 'Wordset was created',
-		wordset: wordset,
-	});
+	Word.findById(req.body.wordId)
+		.then(word => {
+			if (!word) {
+				throw new Error('Word not found');
+			}
+			const wordset = new Wordset({
+				elements: req.body.elements,
+				word: req.body.wordId,
+			});
+
+			return wordset.save();
+		})
+		.then(result => {
+			console.log(result);
+			res.status(201).json({
+				message: 'Wordset stored',
+				createdWordset: {
+					_id: result._id,
+					word: result.word,
+					elements: result.elements,
+				},
+				request: {
+					type: 'GET',
+					url: process.env.APP_URL + '/wordsets/' + result._id,
+				},
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			let statusCode = 500;
+			let errorMessage = 'Internal Server Error';
+
+			if (err.message === 'Word not found') {
+				statusCode = 404;
+				errorMessage = err.message;
+			}
+
+			res.status(statusCode).json({
+				error: errorMessage,
+			});
+		});
 });
 
 router.get('/:wordsetId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Wordset details',
-		wordsetId: req.params.wordsetId,
-	});
+	Wordset.findById(req.params.wordsetId)
+		.select('-__v')
+		.populate('word', '-__v')
+		.exec()
+		.then(wordset => {
+			if (!wordset) {
+				throw new Error('Wordset not found');
+			}
+			res.status(200).json({
+				wordset,
+				request: {
+					type: 'GET',
+					url: process.env.APP_URL + '/wordsets',
+				},
+			});
+		})
+		.catch(err => {
+			let statusCode = 500;
+			let errorMessage = 'Internal Server Error';
+
+			if (err.message === 'Wordset not found') {
+				statusCode = 404;
+				errorMessage = err.message;
+			}
+
+			res.status(statusCode).json({
+				error: errorMessage,
+			});
+		});
 });
 
 router.delete('/:wordsetId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Wordset deleted',
-		wordsetId: req.params.wordsetId,
-	});
+	Wordset.deleteOne({ _id: req.params.wordsetId })
+		.exec()
+		.then(wordset => {
+			res.status(200).json({
+				message: 'Wordset deleted',
+				request: {
+					type: 'POST',
+					url: process.env.APP_URL + '/wordsets',
+					body: {
+						wordId: 'ID',
+						elements: 'Number',
+					},
+				},
+			});
+		})
+		.catch(err => {
+			res.status(500).json({ error: err });
+		});
 });
